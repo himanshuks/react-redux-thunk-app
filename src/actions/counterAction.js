c1e82488a1a8a811d5f91eced0d196a93fdb73ee
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-10-10 13:56:27 +0530

    created action for counter

---
+++ src/actions/counterAction.js
@@ -0,0 +1,13 @@
+// To increment the COUNTER value by 1
+export const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+// To decrement the COUNTER value by 1
+export const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
