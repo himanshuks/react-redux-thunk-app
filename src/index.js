5534a0b1c9dff5d93b85b546a93f2eaad8030488
Himanshu Kumar Singh <himanshu.kr5294@gmail.com>
2021-10-12 12:27:07 +0530

    added logger and reducer in index page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// Thunk is the middleware library
+import thunk from "redux-thunk";
+
+// This will provide Redux dev tools in browser to track the state changes
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// This library can be used to console redux logs
+// It includes Previous state, Action called, Next state
+import logger from "redux-logger";
+
+// import reducer from "./reducers/index";
+import reducers from "./reducers/index";
+
+// Below function will return NEXT function which will return ACTION function
+// Returning NEXT function by passing action means proceed to next ACTION
+// This can be the next MIDDLEWARE or REDUCER
+const myLogger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("Middleware ran...");
+      return next(action);
+    };
+  };
+};
+
+// Above function can also be defined as below syntax
+// It uses concatenated arrow functions
+const crispMyLogger = (store) => (next) => (action) => {
+  console.log("Middleware created using concatenated arrow functions...");
+  return next(action);
+};
+
+// Below function will increment till 5, then decrements the value on NEXT click
+const maxAtFive = (store) => (next) => (action) => {
+  console.log("Stop the counter at Five...");
+  console.log(`Store items ->`, store);
+
+  if (store.getState().count >= 5) return next({ type: "DECREMENT" });
+  return next(action);
+};
+
+// Store is created from REDUX library
+// Order of middleware defines the sequence of running middleware
+// Wrapping the middleware with redux dev tools
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(
+    applyMiddleware(myLogger, crispMyLogger, maxAtFive, logger, thunk)
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
